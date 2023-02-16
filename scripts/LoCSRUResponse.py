fcd4b811b32c5f252fe96a2a9a52d8cdb73ef025
Dataliberate <rjw@dataliberate.com>
2023-02-16 17:07:57 +0000

    Version 2.0
    ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- scripts/LoCSRUResponse.py
+++ scripts/LoCSRUResponse.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import urllib
+#import urllib
 import xml.dom.minidom
+from urllib.request import urlopen
 
-class LoCSRUResponse:
-    def process(self,source):
-        ret = None
-        try:
-            doc = xml.dom.minidom.parse(urllib.urlopen(source))
-            rdfnodes = doc.getElementsByTagNameNS("http://www.w3.org/1999/02/22-rdf-syntax-ns#","RDF")
-            if not rdfnodes:
-                print("LoCSRUResponse - XML Load error: Cannot identify RDF:rdf node in source")
-            else:
-                ret = rdfnodes[0].toxml()
-        except Exception as e:
-            print("LoCSRUResponse - XML Load error: %s" % e)
-        return ret
+def process(source):
+    ret = None
+    try:
+        doc = xml.dom.minidom.parse(source)
+        rdfnodes = doc.getElementsByTagNameNS("http://www.w3.org/1999/02/22-rdf-syntax-ns#","RDF")
+        if not rdfnodes:
+            print("LoCSRUResponse - XML Load error: Cannot identify RDF:rdf node in source")
+        else:
+            ret = rdfnodes[0].toxml()
+    except Exception as e:
+        print("LoCSRUResponse - XML Load error: %s" % e)
+    return ret
+ 
